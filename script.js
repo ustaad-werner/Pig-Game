6e78124180ac862b174d4caf93a9d5a122e5fc8b
ustaad-werner <146668419+ustaad-werner@users.noreply.github.com>
2025-05-10 14:09:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+/*
+
+FUNTIONALITY OF THE GAME -
+* roll the dice and hold the highest score which will be equal to the number on dice.
+* Automatically reset the score when the number comes as 1 on the dice.
+
+* Winning part of the game : the first player to reach the score 100 will win the game.
+
+* CTAs(buttons) are : `New game` `Roll dice` `Hold` alson the dice which when clicked will trigger an event of rolling a random number.
+
+- Highest score to win = 100;
+- user score will be reset `if` one
+
+HOLD FUNCTIONALITY:----->
+
+the current user can hold at a certain score 
+if (score >= 100){
+curretPlayer wins.
+}else{
+switch to the opponentPlayer 
+}
+
+*/ 
